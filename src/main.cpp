0748a1136dae943a54fe1549512ad3aeb87bcae9
user95401 <user95401@gmail.com>
2024-04-11 21:57:53 +0400

    huh

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <Geode/Geode.hpp>
 using namespace geode::prelude;
-
-$execute {
-	CC
-}
+CCLayer* firstEverLayer = nullptr;
+#include <Geode/modify/CCLayer.hpp>
+class $modify(CCLayerExt, CCLayer) {
+    static CCLayer* create(void) {
+        auto rtn = CCLayer::create();
+        //first layer created catch to add search path
+        if (!firstEverLayer) {
+            firstEverLayer = rtn;
+            CCFileUtils::sharedFileUtils()->addPriorityPath((dirs::getTempDir() / "Resources").string().c_str());
+        }
+    }
+};
